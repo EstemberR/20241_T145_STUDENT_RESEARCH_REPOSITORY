3a29280edceb0a6086232abda250a72863711102
EstemberR <22011015892student.buksu.edu.ph>
2024-10-27 08:07:11 +0800

    Progress, Butanganan pa ug login with google

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import Home from  "./Components/Home"
+import Login from "./Components/Login"
+import {BrowserRouter as Router , Routes, Route, } from "react-router-dom";
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/Home" element={<Home/>}/>
+      </Routes>
+    </Router>
+  </div>
+  );
+}
+
+export default App;
