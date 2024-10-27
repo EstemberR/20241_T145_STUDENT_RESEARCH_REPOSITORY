72172512226e97a2da8e24dc5a1a9c9ae43cd31d
[Nezer] <[2201102944@student.buksu.edu.ph]>
2024-10-27 10:11:39 +0800

    design sa login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import Home from  "./Components/Home"
 import Login from "./Components/Login"
 import {BrowserRouter as Router , Routes, Route, } from "react-router-dom";
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
