014fd5265ad1b45816bc82c8a75abe1c05292210
[Nezer] <[2201102944@student.buksu.edu.ph]>
2024-10-28 18:08:40 +0800

    dashboards

---
+++ frontend/src/Components/dashboard/student_dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../component-css/student_dashboard.css'; 
+
+const student_dashboard = () => {
+  return (
+    <h1>Welcome to the Student Dashboard</h1>
+
+  );
+};
+
+export default student_dashboard;
