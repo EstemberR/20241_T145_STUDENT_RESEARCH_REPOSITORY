2c3daae0141e8b5824b74afb614fcbf97d3a9e81
EstemberR <22011015892student.buksu.edu.ph>
2024-10-29 02:54:36 +0800

    Cleaner Version

--- frontend/src/Components/dashboard/admin_dashboard.js
+++
@@ -1,6 +0,0 @@
-
-const adminDashboard = () => {
-    return <h1>Welcome to the Admin Dashboard</h1>;
-  };
-  
-  export default adminDashboard;
---
+++ server/src/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import './firebaseAdminConfig.js';
+import authRoutes from './authRouts.js'; 
+
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+const PORT = process.env.PORT || 8000; 
+
+// Middleware
+app.use(cors()); // Make sure you have cors middleware if needed
+app.use(express.json()); // Enable JSON parsing for requests
+
+// Connect to MongoDB
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Connected to MONGODB');
+    } catch (error) {
+        console.error('Error connecting to MONGODB:', error);
+    }
+};
+
+// MongoDB connection events
+mongoose.connection.on('disconnected', () => {
+    console.log('Disconnected from MONGODB');
+});
+
+// API Routes
+app.use('/api/auth', authRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+    connect(); // Call connect to establish MongoDB connection
+    console.log(`Listening on PORT ${PORT}`);
+});
