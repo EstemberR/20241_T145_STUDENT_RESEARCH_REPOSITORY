eb29868c73425fdd9685536d7e36de36f8361b76
EstemberR <22011015892student.buksu.edu.ph>
2024-10-29 10:16:48 +0800

    Updated

--- client/vite-project/src/components/firebaseConfig.js
+++ client/vite-project/src/components/firebaseConfig.js
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyACPglO2oMslerC4Mrnpz-xNpSb19EUzwI",
-  authDomain: "student-research-repository.firebaseapp.com",
-  projectId: "student-research-repository",
-  storageBucket: "student-research-repository.appspot.com",
-  messagingSenderId: "387430837730",
-  appId: "1:387430837730:web:742a628911906b5b545dbb"
+//hiding the API information
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId:import.meta.env.VITE_APP_ID
 };
 
 
--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import './firebaseAdminConfig.js';
 import authRoutes from './authRouts.js'; 
+import express from 'express'
+import bodyParser from 'body-parser';
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -13,6 +15,7 @@ const PORT = process.env.PORT || 8000;
 // Middleware
 app.use(cors()); 
 app.use(express.json()); // Enable JSON parsing for requests
+app.use(bodyParser.json());
 
 // Connect to MongoDB
 const connect = async () => {
