3a08e6fea46db2ad1cadea3dbce1221cd8d47b9b
EstemberR <22011015892student.buksu.edu.ph>
2024-10-31 11:36:24 +0800

    adding services for firebaseconfig

--- client/vite-project/src/components/firebaseConfig.js
+++ client/vite-project/src/components/firebaseConfig.js
@@ -1,19 +1,10 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
+// web app's Firebase configuration
+import firebaseConfig from '../services/firebase'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// web app's Firebase configuration
-const firebaseConfig = {
-apiKey: "AIzaSyACPglO2oMslerC4Mrnpz-xNpSb19EUzwI",
-authDomain: "student-research-repository.firebaseapp.com",
-projectId: "student-research-repository",
-storageBucket: "student-research-repository.appspot.com",
-messagingSenderId: "387430837730",
-appId: "1:387430837730:web:742a628911906b5b545dbb"
-};
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
