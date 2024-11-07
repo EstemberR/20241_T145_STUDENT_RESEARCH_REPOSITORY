694d240c1f8fcd2bade3c55b1033899c19d29b9f
[Nezer] <[2201102944@student.buksu.edu.ph]>
2024-11-07 19:45:26 +0800

    dyanmic name display[student, instructor] + user profile routes

--- server/model/Student.js
+++ server/model/Student.js
@@ -1,12 +1,13 @@
 // Student.js
 import mongoose from 'mongoose';
-import User from './user.js'; // Import the base User schema
+import User from './user.js'; 
 
 const studentSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     uid: { type: String, required: true, unique: true },
-    role: { type: String, enum: ['student'], required: true } 
+    course: {type: String, required: false },
+    role: { type: String, enum: ['student'], required: true }
 });
 
 const Student = mongoose.model('Student', studentSchema);
