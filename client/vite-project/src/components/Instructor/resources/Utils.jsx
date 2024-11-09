b86bc3bdea69683ad481a46857369838509bf45c
[Nezer] <[2201102944@student.buksu.edu.ph]>
2024-11-09 12:44:32 +0800

    arrange instructor + profile

---
+++ client/vite-project/src/components/Instructor/resources/Utils.jsx
@@ -0,0 +1,7 @@
+export const getUserName = () => localStorage.getItem('userName');
+export const getToken = () => localStorage.getItem('token');
+export const handleLogout = (navigate) => {
+  localStorage.removeItem('userName');
+  localStorage.removeItem('token');
+  navigate('/');
+};
