d1d442cb81ead577ba53fab3d4acf3dcc822c306
[Nezer] <[2201102944@student.buksu.edu.ph]>
2024-11-10 11:34:04 +0800

    +4 routes for admin

--- server/model/Student.js
+++ server/model/Student.js
@@ -1,13 +1,14 @@
 // Student.js
 import mongoose from 'mongoose';
-import User from './user.js'; 
 
 const studentSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     uid: { type: String, required: true, unique: true },
     course: {type: String, required: false },
-    role: { type: String, enum: ['student'], required: true }
+    role: { type: String, enum: ['student'], required: true },
+    studentId: { type: String, required: true, unique: true },
+    archived: { type: Boolean, default: false }
 });
 
 const Student = mongoose.model('Student', studentSchema);
